71cd3e244df04347dca0f8dcaf0247574fed65ee
Carl Persson <carl@j35.se>
2023-11-03 03:09:26 +0100

    Sampled the last 50 000 unconditional EDM images from pre-trained CIFAR-10 model

--- helper.py
+++ helper.py
@@ -11,6 +11,7 @@ def combine_npz(file_names, num_batches, save_name, conditional):
         file = np.load(files)
         if images is None or labels is None:
             images = file['images']
+            labels = 0
             if conditional:
                 labels = file['labels']
         else:
@@ -33,14 +34,15 @@ if __name__ == '__main__':
     if combine is True:
         # Combine into 5 batches with 10000 images and labels in each batch
         num_files = 200
-        file_names = ['training_data/cond_samples_batch' + str(i) + '.npz' for i in range(num_files)]
-        save_name = 'training_data/cond_samples'
-        combine_npz(file_names, 5, save_name, True)
+        file_names = ['training_data/uncond_samples_batch' + str(i) + '.npz' for i in range(num_files)]
+        save_name = 'training_data/uncond_samples'
+        combine_npz(file_names, 5, save_name, False)
     else:
         # Check such that the combined batches are not duplicates
-        file1 = np.load('training_data/cond_samples1.npz')
-        file2 = np.load('training_data/cond_samples2.npz')
-        plt.imshow(file1['images'][10])
+        file1 = np.load('training_data/uncond_samples0.npz')
+        file2 = np.load('training_data/uncond_samples1.npz')
+        print(file1['images'].shape, file2['images'].shape)
+        plt.imshow(file1['images'][0])
         plt.show()
-        plt.imshow(file2['images'][10])
+        plt.imshow(file2['images'][0])
         plt.show()
