571a158a5ccb6dab28ebfbe5eabf05aef7b9c1a0
Carl Persson <carl@j35.se>
2023-11-11 18:27:08 +0100

    Re-sampled the EDM into one file of 50 000 each. Implemented the dataloader and loss function for the training loop. Main training loop needs to be implemented.

--- combine.py
+++ combine.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pickle
 
 
 # Quick helper function to combine npz files
@@ -10,12 +11,12 @@ def combine_npz(file_names, num_batches, save_name, conditional):
     for i, files in enumerate(file_names):
         file = np.load(files)
         if images is None or labels is None:
-            images = file['images']
+            images = file['data']
             labels = 0
             if conditional:
                 labels = file['labels']
         else:
-            images = np.concatenate((images, file['images']))
+            images = np.concatenate((images, file['data']))
             if conditional:
                 labels = np.concatenate((labels, file['labels']))
 
@@ -30,19 +31,18 @@ def combine_npz(file_names, num_batches, save_name, conditional):
 
 
 if __name__ == '__main__':
-    combine = False
+    combine = True
     if combine is True:
         # Combine into 5 batches with 10000 images and labels in each batch
-        num_files = 200
-        file_names = ['training_data/uncond_samples_batch' + str(i) + '.npz' for i in range(num_files)]
-        save_name = 'training_data/uncond_samples'
-        combine_npz(file_names, 5, save_name, False)
+        num_files = 5
+        file_names = ['training_data/data_batch_' + str(i) for i in range(num_files)]
+        save_name = 'training_data/edm_uncond_samples'
+        combine_npz(file_names, 1, save_name, True)
     else:
         # Check such that the combined batches are not duplicates
-        file1 = np.load('training_data/uncond_samples0.npz')
-        file2 = np.load('training_data/uncond_samples1.npz')
-        print(file1['images'].shape, file2['images'].shape)
-        plt.imshow(file1['images'][0])
+        file = np.load('training_data/edm_uncond_samples0.npz')
+        print(file['images'].shape)
+        plt.imshow(file['images'][0])
         plt.show()
-        plt.imshow(file2['images'][0])
+        plt.imshow(file['images'][250])
         plt.show()
--- train.py
+++ train.py
@@ -1,4 +1,113 @@
 import torch
 import numpy as np
+import click
+import torch.utils.data as data
+import torchvision.transforms as transforms
+from discriminator import get_discriminator_model, get_ADM_model
+from keras.datasets import cifar10
+import dnnlib
 
 # TODO: Create training run according to the paper
+
+
+def BCE_loss_fun(temporal_weight=None):
+    """
+
+    Args:
+        temporal_weight: Function that takes t as input as returns the corresponding weight
+
+    Returns: Weighted BCE loss function
+
+    """
+    def loss_fun(pred, labels, t=None):
+        """
+
+        Args:
+            pred: B vector of model predictions
+            labels: B vector of true labels
+            t: B vector of diffusion times used to calculate the weight
+
+        Returns: The BCE loss weighted with the temporal weight
+
+        """
+        if temporal_weight is None or t is None:
+            loss = (torch.matmul(labels, torch.log(pred)) + torch.matmul(1 - labels, torch.log(1 - pred)))
+        else:
+            loss = torch.mul(temporal_weight(t), (torch.matmul(labels, torch.log(pred)) +
+                                                  torch.matmul(1 - labels, torch.log(1 - pred))))
+        return -torch.mean(loss)
+    return loss_fun
+
+
+class CustomDataset(data.Dataset):
+    def __init__(self, data, labels, cond=None):
+        self.data = data
+        self.labels = labels
+        self.cond = cond
+
+    def __getitem__(self, item):
+        if self.cond is None:
+            return self.data[item], self.labels[item]
+        else:
+            return self.data[item], self.labels[item], self.cond[item]
+
+    def __len__(self):
+        return len(self.data)
+
+
+@click.command()
+@click.option('--sample_dir',                  help='Save directory',         metavar='PATH',    type=str, required=True,     default="training_data/conditional_edm_samples/edm_cond_samples.npz")
+@click.option('--cond',                        help='Is it conditional?',     metavar='BOOL',    type=click.IntRange(min=0),  default=0)
+@click.option('--batch_size',                  help='Batch size',             metavar='INT',     type=click.IntRange(min=1),  default=128)
+@click.option('--n_epochs',                    help='Num epochs',             metavar='INT',     type=click.IntRange(min=1),  default=50)
+@click.option('--lr',                          help='Learning rate',          metavar='FLOAT',   type=click.FloatRange(min=0),default=3e-4)
+@click.option('--wd',                          help='Weight decay',           metavar='FLOAT',   type=click.FloatRange(min=0),default=0)
+def main(**kwargs):
+    # Load the arguments
+    opts = dnnlib.EasyDict(kwargs)
+
+    # Load images and discriminator labels
+    true_data, true_cond = cifar10.load_data()[0]
+    true_data = torch.from_numpy(true_data)
+
+    fake_data = torch.from_numpy(np.load(opts.sample_dir)['images'])
+    training_data = torch.concatenate((true_data, fake_data))
+    training_lbl = torch.concatenate((torch.ones(true_data.shape[0]), torch.zeros(fake_data.shape[0])))
+
+    # If conditioned on class, load those aswell
+    if opts.cond:
+        true_cond = np.eye(10)[true_cond.squeeze()]
+        fake_cond = np.load(opts.sample_dir)['labels']
+        training_cond = torch.from_numpy(np.concatenate((true_cond, fake_cond)))
+        # Create a data set that returns each item
+        dataset = CustomDataset(training_data, training_lbl, training_cond)
+    else:
+        dataset = CustomDataset(training_data, training_lbl)
+
+    # Set up training and custom loss function.
+    loss_fun = BCE_loss_fun()
+    adm_classifier = get_ADM_model()
+    discriminator = get_discriminator_model(opts.cond)
+    optimizer = torch.optim.Adam(discriminator.parameters(), lr=opts.lr, weight_decay=opts.wd)
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=opts.batch_size, shuffle=True)
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    for i in range(opts.n_epochs):
+        for samples in data_loader:
+            # Reset grads
+            optimizer.zero_grad()
+
+            # Load samples into device memory
+            if opts.cond:
+                image, label, cond = samples
+                cond = cond.to(device)
+            else:
+                image, label = samples
+            image = image.to(device)
+            label = label.to(device)
+
+            # TODO: Implement Loss calculations here!
+
+
+if __name__ == "__main__":
+    main()
